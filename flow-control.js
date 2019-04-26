b6bebf342a1521bce8ee47a3ee67ce5a096deb01
Tejashri Parurkar <tparurkar@gmail.com>
2019-04-26 19:23:10 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,51 @@
-function basicTeenager(age) {
 
+function basicTeenager()
+{
+  var age = 13
+  if( 13 <= age <= 19 )
+  {
+    return "You are a teenager!"
+  }
 }
 
-function teenager(age) {
-
+function teenager(age)
+{
+  if( 13 <= age <= 19 )
+  {
+    return "You are a teenager!"
+  }
+  else
+  {
+      return "You are not a teenager"
+  }
 }
 
-function ageChecker(age) {
+teenager(20)
 
+
+function ageChecker(age)
+{
+if( 13 <= age <= 19 )
+{
+  return "You are a teenager!"
 }
-
-function ternaryTeenager(age) {
-
+else if ( age <= 12 )
+{
+return "You are a kid"
 }
-
-function switchAge(age) {
-
+else
+{
+return "You are grownup"
 }
+}
+ageChecker(30)
+
+function ternaryTeenager(age)
+{
+  var age
+return 13<=age<=19 ?  "You are a teenager!" : "You are not a teenager"
+}
+ ternaryTeenager(60)
+/*function switchAge(age) {
+
+}*/
